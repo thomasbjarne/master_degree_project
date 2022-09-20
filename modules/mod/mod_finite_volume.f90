1860af4f402a009c277ec3853b1981417126baf8
Thomas Bjarne Hestvik <110252557+thomasbjarne@users.noreply.github.com>
2022-09-20 20:56:14 +0200

    Add files via upload

--- modules/mod/mod_finite_volume.f90
+++ modules/mod/mod_finite_volume.f90
@@ -28,48 +28,6 @@ contains
 
     end function upwind_scheme
 
-    pure function naive_central_scheme(t,y) result(res)
-
-        real, intent(in), dimension(:) :: y
-        real, intent(in), optional :: t
-        real, dimension(size(y)) :: res
-        integer :: i
-        real, parameter :: a = -1.
-        real, parameter :: h = 1./500
-
-        res(1) = -(a/(2*h))*(y(2)-y(size(y)))
-        do i = 2, size(y)-1
-            res(i) = -(a/(2*h))*(y(i+1)-y(i-1))
-        end do
-        res(size(y)) = -(a/(2*h))*(y(1)-y(size(y)-1))
-
-    end function naive_central_scheme
-
-    pure function Lax_Friedrichs_scheme(t,y) result(res)
-
-        real, intent(in), dimension(:) :: y
-        real, intent(in), optional :: t
-        real, dimension(size(y)) :: res
-        integer :: i
-        real, parameter :: a = -1.
-        real, parameter :: h = 1./500
-
-        res(1) = -(a/(2*h))*(y(2)-y(size(y))) + (0.5)*(y(2) + y(size(y)))
-        do i = 2, size(y)-1
-            res(i) = -(a/(2*h))*(y(i+1)-y(i-1)) + (0.5)*(y(i+1)+y(i-1))
-        end do
-        res(size(y)) = -(a/(2*h))*(y(1)-y(size(y)-1)) + (0.5)*(y(1) + y(size(y)-1))
-
-    end function Lax_Friedrichs_scheme
-
-    pure function godunov_scheme(t,y) result(res)
-
-        real, intent(in), dimension(:) :: y
-        real, intent(in), optional :: t
-        real, dimension(size(y)) :: res
-
-    end function godunov_scheme
-
 !FULLY DISCRETE SCHEMES
 
     pure function fully_discrete_upwind_scheme(h, speed, tmin, tmax, Q_0) result(Q)
@@ -165,5 +123,54 @@ contains
         end do
 
     end function fully_discrete_Richtmyer_LW_scheme
+
+    pure function fully_discrete_second_order_LW_scheme(h, speed, tmin, tmax, Q_0) result(Q)
+
+        real, intent(in) :: h, speed, tmin, tmax
+        real, intent(in), dimension(:) :: Q_0
+        real, dimension(:,:), allocatable :: Q
+        real :: k
+        integer :: i, n
+        n = size(Q_0)
+        k = abs(tmax - tmin)/n
+        allocate( Q(n, n) )
+
+        Q(1,:) = Q_0
+        do i = 1, n-1
+            Q(i+1, 1) = Q(i, 1) - (0.5)*((speed*k)/(h))*(Q(i, 2) - Q(i, n)) &
+            + (0.5)*((speed**2)*(k**2)/(h**2))*(Q(i,2) - 2*Q(i, 1) + Q(i, n))
+            Q(i+1, 2:n-1) = Q(i, 2:n-1) - (0.5)*((speed*k)/(h))*(Q(i, 3:n) - Q(i, 1:n-2)) &
+            + (0.5)*((speed**2)*(k**2)/(h**2))*(Q(i,3:n) - 2*Q(i, 2:n-1) + Q(i, 1:n-2))
+            Q(i+1, n) = Q(i, n) - (0.5)*((speed*k)/(h))*(Q(i, 1) - Q(i, n-1)) &
+            + (0.5)*((speed**2)*(k**2)/(h**2))*(Q(i,1) - 2*Q(i, n) + Q(i, n-1))
+        end do
+
+    end function fully_discrete_second_order_LW_scheme
+
+    pure function fully_discrete_second_order_LW_scheme_1dsys(h, A, tmin, tmax, Q_0) result(Q)
+
+        real, intent(in) :: h, tmin, tmax
+        real, intent(in), dimension(:,:) :: Q_0, A
+        real, dimension(:,:,:), allocatable :: Q
+        real :: k
+        integer :: i, n, j
+        n = size(Q_0,1)
+        k = abs(tmax - tmin)/n
+        allocate( Q(n, n, size(Q_0,2)) )
+
+        Q(1,:,:) = Q_0(:,:)
+        do i = 1, n-1
+            Q(i+1, 1, :) = Q(i, 1, :) - matmul(A, (0.5)*(k/h)*(Q(i, 2, :) - Q(i, n, :))) &
+            + (0.5)*matmul(matmul(A,A), ((k**2)/(h**2))*(Q(i,2, :) - 2*Q(i, 1, :) + Q(i, n, :)))
+
+            do j = 2, n-1
+                Q(i+1, j, :) = Q(i, j, :) - matmul(A, (0.5)*(k/h)*(Q(i, j+1, :) - Q(i, j-1, :))) &
+                + matmul(matmul(A, A), (0.5)*((k**2)/(h**2))*(Q(i, j+1, :) - 2*Q(i, j, :) + Q(i, j-1, :)))
+            end do
+            Q(i+1, n, :) = Q(i, n, :) - matmul(A,(0.5)*(k/h)*(Q(i, 1, :) - Q(i, n-1, :))) &
+            + matmul(matmul(A,A), (0.5)*((k**2)/(h**2))*(Q(i,1, :) - 2*Q(i, n, :) + Q(i, n-1, :)))
+        end do
+
+    end function fully_discrete_second_order_LW_scheme_1dsys
 
 end module mod_finite_volume
--- modules/test.f90
+++ modules/test.f90
@@ -13,29 +13,34 @@ program test
 	use mod_finite_volume
     implicit none
     
-    ! We attempt to solve q_t + aq_x = 0,
-    ! with u_0 = sin(2pi*x) and periodic boundary,
+    ! We attempt to solve q_t + Aq_x = 0,
+    ! with (q_i)_0 = sin((2i-1)pi*x) and periodic boundary,
     ! using a simple finite volume method.
     
 	integer, parameter :: n = 500
-	integer :: fileunit, i, j, k
-	real, dimension(0:n) :: x, time
-	real, dimension(1:n) :: vol_avg_q_0
-	real, dimension(:,:), allocatable :: vol_avg_q
+	integer :: i, j, k
+	real, dimension(0:n) :: x
+	real, dimension(1:n, 2) :: vol_avg_q_0
+	real, dimension(:,:, :), allocatable :: vol_avg_q
+	real, dimension(2, 2) :: A
 	real, parameter :: pi = 3.14159265358979323846
 	real :: h = 1./(n)
 	do i = 0, n 
 		x(i) = i*h
 	end do
-	
+	A = 0
+	A(1,1) = 1
+	A(2,2) = -1
 	do i = 1, n
-		vol_avg_q_0(i) = (1./(2*pi*h)) * (cos(2*pi*x(i-1)) - cos(2*pi*x(i)))
+		do j = 1, 2
+			vol_avg_q_0(i, j) = (1./(2*pi*h)) * (cos((2*j -1)*pi*x(i-1)) - cos((2*j-1)*pi*x(i)))
+		end do
 	end do
 	
-	vol_avg_q = fully_discrete_Richtmyer_LW_scheme(h=h, speed=1., tmin=0., tmax=1., q_0=vol_avg_q_0)
+	vol_avg_q = fully_discrete_second_order_LW_scheme_1dsys(h=h, a=A, tmin=0., tmax=1., q_0=vol_avg_q_0)
 
 	print*, 'Maxval of Q is', maxval(vol_avg_q)
 
-	call write_to_file(vol_avg_q, 1)
+	call write_to_file(vol_avg_q(1,:,:), 1)
 
 end program test
